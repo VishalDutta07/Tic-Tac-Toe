026f2f3c82b027870dc8346862b593f5958f8e84
Vishal Dutta <141305006+VishalDutta07@users.noreply.github.com>
2025-01-02 12:09:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,76 @@
+let boxes = document.querySelectorAll(".box");
+let resetBtn = document.querySelector("#reset");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
+let turnO = true;
+
+const winpattern = [
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [3, 4, 5],
+    [6, 7, 8],
+    [2, 4, 6]
+];
+
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO) {
+            box.innerText = "O";
+            turnO = false;
+        } else {
+            box.innerText = "X";
+            turnO = true;
+        }
+        box.disabled = true;
+
+        checkwinner();
+    });
+});
+
+const showWinner = (winner) => {
+    msg.innerText = `Winner is ${winner}`;
+    msgContainer.classList.remove("hide");
+    disableBoxes();
+};
+
+const disableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = true;
+    });
+};
+
+const enableBoxes = () => {
+    boxes.forEach((box) => {
+        box.disabled = false;
+        box.innerText = ""; // Clear the text in each box
+    });
+};
+
+const resetGame = () => {
+    turnO = true;
+    enableBoxes();
+    msgContainer.classList.add("hide");
+};
+
+const checkwinner = () => {
+    for (let pattern of winpattern) {
+        const [a, b, c] = pattern;
+        if (boxes[a].innerText && boxes[a].innerText === boxes[b].innerText && boxes[a].innerText === boxes[c].innerText) {
+            showWinner(boxes[a].innerText);
+            return;
+        }
+    }
+    // Check for a draw
+    if ([...boxes].every(box => box.innerText)) {
+        msg.innerText = "It's a draw!";
+        msgContainer.classList.remove("hide");
+        disableBoxes();
+    }
+};
+
+resetBtn.addEventListener("click", resetGame);
+newGameBtn.addEventListener("click", resetGame);
